bb6909fdf090c05cb4b5870f8abca1e6ed31f7f4
JuanchoAguirrre <s3776171@student.rmit.edu.au>
2022-02-03 09:52:01 +1100

    reattempt at react struct #3.1incl visitCount functionality

--- react-files-2/src/App.js
+++ react-files-2/src/App.js
@@ -1,15 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import Resume from './components/Resume/';
-import Header from './components/Header/';
+import Resume from './components/Resume';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit Test! <code>src/App.js</code> and save to reload.
+          Edit Test.
         </p>
         <Header />
         <Resume />
