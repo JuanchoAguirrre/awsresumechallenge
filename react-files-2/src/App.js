277dd0de1f3b47a1d4ce5355ae96dcc6b733849b
JuanchoAguirrre <s3776171@student.rmit.edu.au>
2022-02-03 09:48:50 +1100

    reattempt at react struct #3.0 incl visitCount functionality

--- react-files-2/src/App.js
+++ react-files-2/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import Resume from './components/Resume/';
+import Header from './components/Header/';
 
 function App() {
   return (
@@ -9,14 +11,9 @@ function App() {
         <p>
           Edit Test! <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Header />
+        <Resume />
+
       </header>
     </div>
   );
