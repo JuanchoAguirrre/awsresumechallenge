df7d395ef5420a528bbb62f4384f572f2197e97a
JuanchoAguirrre <s3776171@student.rmit.edu.au>
2022-02-07 16:53:41 +1100

    pushing recent react changes 1.1

---
+++ react-files-2/src/components/Body/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+const Resume = () => {
+
+    return (
+        <div className="Resume">
+            <div>
+                <h1>Blog:</h1>
+                <h3>
+                    Hi all! I spent quite some time developing this website so 
+                    take a tour, as it was all built using serverless Amazon web services
+                </h3>
+            </div>
+        </div>
+    )
+}
+
+export default Resume
