463a208f75cfa94872d1d4f723458f48c127f27d
JuanchoAguirrre <s3776171@student.rmit.edu.au>
2022-02-16 11:39:57 +1100

    react files update 1.8: pdf being displayed from local, test from web

--- react-files-2/src/components/Resume/index.js
+++ react-files-2/src/components/Resume/index.js
@@ -1,26 +1,9 @@
 
 import React from 'react';
 import {Document, Page, pdfjs} from 'react-pdf'
-import { PDFViewer, View, Text, StyleSheet } from '@react-pdf/renderer';
 import resumePDF from '../../public/resume.pdf';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
-    
-   
-
-const pdfStyles = StyleSheet.create({
-    viewer: {
-        height: window.innerHeight /2 ,
-        width: window.innerWidth / 3,
-    },
-    page: { 
-        flexDirection: 'row' 
-    },
-    section: { 
-        textAlign: 'center',
-        margin: 30 
-    }
-});
 
 
 const Resume = () => (
@@ -37,8 +20,5 @@ const Resume = () => (
                 />
         </Document>
 );
-
-
-// ReactPDF.renderToFile(<Resume/>)
 
 export default Resume;
